7635f49dd8d0420a260ecad2b3e939e2bcee437e
Davide Pulci <davide.pulci@gmail.com>
2022-05-26 19:04:04 +0200

    fine

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -104,5 +104,9 @@ class ComicController extends Controller
     public function destroy($id)
     {
         //
+        $comic = Comic::find($id);
+        $comic->delete();
+
+        return redirect()->route('comics.index');
     }
 }
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -11,6 +11,15 @@
             <p><b>Sale date:</b> {{ $comic->sale_date }}</p>
             <p><b>Description:</b> {{ $comic->description }}</p>
         </div>
+
+        <form id="form-delete" action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+            @csrf
+
+            @method('DELETE')
+
+            <input type="submit" value="Cancella" class="delete">
+        </form>
+
         <a href="{{ route('comics.index') }}">BACK</a>
     </main>
 @endsection
